155399e6809a1e1d94ac3f3229abe6f58750bcfb
bruno13palhano <bruno13palhano@gmail.com>
2023-05-28 09:57:17 -0300

    Changed ProjectData to Project

--- src/main/java/com/bruno13palhano/portfolio/controllers/ProjectController.java
+++ src/main/java/com/bruno13palhano/portfolio/controllers/ProjectController.java
@@ -1,7 +1,7 @@
 package com.bruno13palhano.portfolio.controllers;
 
-import com.bruno13palhano.model.ProjectData;
-import com.bruno13palhano.repository.ProjectDAO;
+import com.bruno13palhano.Project;
+import com.bruno13palhano.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class ProjectController {
 
     @Autowired
-    private ProjectDAO projectDAO;
+    private ProjectRepository projectRepository;
 
     @PostMapping(path = "/add")
     public @ResponseBody String addNewProject(
@@ -19,22 +19,22 @@ public class ProjectController {
         @RequestParam String type,
         @RequestParam String description
     ) {
-        ProjectData projectData = new ProjectData();
-        projectData.setName(name);
-        projectData.setType(type);
-        projectData.setDescription(description);
-        projectDAO.save(projectData);
+        Project project = new Project();
+        project.setName(name);
+        project.setType(type);
+        project.setDescription(description);
+        projectRepository.insert(project);
 
         return "Saved";
     }
 
     @GetMapping(path = "/all")
-    public @ResponseBody Iterable<ProjectData> getAllProjects() {
-        return projectDAO.findAll();
+    public @ResponseBody Iterable<Project> getAllProjects() {
+        return projectRepository.getAll();
     }
 
     @GetMapping(path = "/{id}")
-    public @ResponseBody ProjectData getProjectById(@PathVariable Integer id) {
-        return projectDAO.findProjectById(id);
+    public @ResponseBody Project getProjectById(@PathVariable Integer id) {
+        return projectRepository.getById(id);
     }
 }
