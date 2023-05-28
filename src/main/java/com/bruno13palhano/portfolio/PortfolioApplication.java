72a58823b1b2e836a86e79a153f18adc07735895
bruno13palhano <bruno13palhano@gmail.com>
2023-05-27 23:26:36 -0300

    Added component scan

--- src/main/java/com/bruno13palhano/portfolio/PortfolioApplication.java
+++ src/main/java/com/bruno13palhano/portfolio/PortfolioApplication.java
@@ -2,8 +2,10 @@ package com.bruno13palhano.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bruno13palhano"})
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
