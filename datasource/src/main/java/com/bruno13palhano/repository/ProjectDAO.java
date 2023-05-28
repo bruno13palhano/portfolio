ec6a6c793cbe9b4022dc7df8ff9877eaef8bc0b0
bruno13palhano <bruno13palhano@gmail.com>
2023-05-28 09:53:59 -0300

    Added a update query to ProjectDAO

--- datasource/src/main/java/com/bruno13palhano/repository/ProjectDAO.java
+++ datasource/src/main/java/com/bruno13palhano/repository/ProjectDAO.java
@@ -8,4 +8,8 @@ import org.springframework.data.repository.query.Param;
 public interface ProjectDAO extends CrudRepository<ProjectData, Integer> {
     @Query(value = "SELECT * FROM project p WHERE p.id = :id", nativeQuery = true)
     ProjectData findProjectById(@Param("id") Integer id);
+
+    @Query(value = "UPDATE project p SET p.name = :pr.name, p.type = :pr.type, pr.description = :description " +
+            "WHERE p.id = :pr.id", nativeQuery = true)
+    void updateProject(@Param("pr") ProjectData project);
 }
