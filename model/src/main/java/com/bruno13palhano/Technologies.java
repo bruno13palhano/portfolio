d82c14da54f02de785af3d90a34177043a91b347
bruno13palhano <bruno13palhano@gmail.com>
2023-05-28 10:47:46 -0300

    Created a enum for projects technologies

---
+++ model/src/main/java/com/bruno13palhano/Technologies.java
@@ -0,0 +1,19 @@
+package com.bruno13palhano;
+
+public enum Technologies {
+    ANDROID,
+    ANGULAR,
+    CSS,
+    HTML,
+    HILT,
+    JAVA,
+    JAVASCRIPT,
+    KOTLIN,
+    MYSQL,
+    NODEJS,
+    POSTGRESQL,
+    ROOM,
+    SPRING,
+    SPRING_BOOT,
+    TYPESCRIPT
+}
