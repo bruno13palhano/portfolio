ebf0edb10727664847e25449fcdfc3bf1f4b5355
bruno13palhano <bruno13palhano@gmail.com>
2023-05-29 15:42:10 -0300

    Adjusted queires

--- datasource/src/main/java/com/bruno13palhano/repository/ProjectDAO.java
+++ datasource/src/main/java/com/bruno13palhano/repository/ProjectDAO.java
@@ -6,10 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface ProjectDAO extends CrudRepository<ProjectData, Integer> {
-    @Query(value = "SELECT * FROM project p WHERE p.id = :id", nativeQuery = true)
+    @Query(value = "SELECT * FROM project_data p WHERE p.id = :id", nativeQuery = true)
     ProjectData findProjectById(@Param("id") Integer id);
 
-    @Query(value = "UPDATE project p SET p.name = :pr.name, p.type = :pr.type, pr.description = :description " +
-            "WHERE p.id = :pr.id", nativeQuery = true)
-    void updateProject(@Param("pr") ProjectData project);
+    @Query(value = "UPDATE project_data p SET p.name = :project.name, p.type = :project.type, p.description = :project.description " +
+            "WHERE p.id = :project.id", nativeQuery = true)
+    void updateProject(@Param("project") ProjectData project);
 }
