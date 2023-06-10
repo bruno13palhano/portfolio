18ca2699ff9203814cdeb145b3ee4ac2e10f28af
bruno13palhano <bruno13palhano@gmail.com>
2023-06-10 07:02:17 -0300

    Add delete project by id query

--- datasource/src/main/java/com/bruno13palhano/repository/ProjectDAO.java
+++ datasource/src/main/java/com/bruno13palhano/repository/ProjectDAO.java
@@ -1,5 +1,6 @@
 package com.bruno13palhano.repository;
 
+import com.bruno13palhano.Project;
 import com.bruno13palhano.model.ProjectData;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,6 +9,9 @@ import org.springframework.data.repository.query.Param;
 public interface ProjectDAO extends CrudRepository<ProjectData, Integer> {
     @Query(value = "SELECT * FROM project_data p WHERE p.id = :id", nativeQuery = true)
     ProjectData findProjectById(@Param("id") Integer id);
+
+    @Query(value = "DELETE FROM project_data p WHERE p.id = :id", nativeQuery = true)
+    Project deleteProjectById(@Param("id") Integer id);
 
     @Query(value = "UPDATE project_data p SET p.name = :project.name, p.type = :project.type, p.description = :project.description " +
             "WHERE p.id = :project.id", nativeQuery = true)
