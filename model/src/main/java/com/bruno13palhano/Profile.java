90ccd1a42841a505744ac3402e736b91c46b28c8
bruno13palhano <bruno13palhano@gmail.com>
2023-06-11 19:42:20 -0300

    Add Profile model

---
+++ model/src/main/java/com/bruno13palhano/Profile.java
@@ -0,0 +1,9 @@
+package com.bruno13palhano;
+
+public class Profile {
+    private Integer id;
+    private String name;
+    private String description;
+    private String email;
+    private String cellphoneNumber;
+}
