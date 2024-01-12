c5cbb98b97a6cdfdf348d71ba8ff3a9e854212d4
bruno13palhano <bruno13palhano@gmail.com>
2024-01-12 20:26:41 -0300

    Add a table for profile

--- datasource/src/main/resources/schema.sql
+++ datasource/src/main/resources/schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS projects (
     images_urls TEXT,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS profile (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    email VARCHAR(255),
+    cellphone_number VARCHAR(55),
+    social_media TEXT,
+    PRIMARY KEY (id)
+)
