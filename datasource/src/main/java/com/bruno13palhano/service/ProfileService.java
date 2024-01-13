e8d75bf37e8f6b5242ef75fce32d443677f0477e
bruno13palhano <bruno13palhano@gmail.com>
2024-01-13 19:53:03 -0300

    Add a service for profile

---
+++ datasource/src/main/java/com/bruno13palhano/service/ProfileService.java
@@ -0,0 +1,5 @@
+package com.bruno13palhano.service;
+
+import com.bruno13palhano.Profile;
+
+public interface ProfileService extends Service<Profile> {}
---
+++ datasource/src/main/java/com/bruno13palhano/service/impl/DefaultProfileService.java
@@ -0,0 +1,40 @@
+package com.bruno13palhano.service.impl;
+
+import com.bruno13palhano.Profile;
+import com.bruno13palhano.repository.ProfileRepository;
+import com.bruno13palhano.service.ProfileService;
+
+import java.util.List;
+
+public class DefaultProfileService implements ProfileService {
+    private final ProfileRepository profileRepository;
+
+    public DefaultProfileService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    @Override
+    public void insert(Profile model) {
+        profileRepository.insert(model);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        profileRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(Profile model) {
+        profileRepository.update(model);
+    }
+
+    @Override
+    public List<Profile> getAll() {
+        return profileRepository.getAll();
+    }
+
+    @Override
+    public Profile getById(Integer id) {
+        return profileRepository.getById(id);
+    }
+}
