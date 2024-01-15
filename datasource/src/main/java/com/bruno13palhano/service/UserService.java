d28e80cbc4a2af12e16b25749646af4d5f542569
bruno13palhano <bruno13palhano@gmail.com>
2024-01-15 19:14:40 -0300

    Change interface implementtion to UserData

--- datasource/src/main/java/com/bruno13palhano/service/UserService.java
+++ datasource/src/main/java/com/bruno13palhano/service/UserService.java
@@ -1,5 +1,6 @@
 package com.bruno13palhano.service;
 
 import com.bruno13palhano.User;
+import com.bruno13palhano.UserData;
 
-public interface UserService extends Service<User> {}
+public interface UserService extends UserData<User> {}
