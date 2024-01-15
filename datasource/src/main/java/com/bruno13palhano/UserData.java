7bbd00a0adc8d18e9e4d1f2ab51adf6d4e9f4250
bruno13palhano <bruno13palhano@gmail.com>
2024-01-15 19:05:32 -0300

    Fix id type

--- datasource/src/main/java/com/bruno13palhano/UserData.java
+++ datasource/src/main/java/com/bruno13palhano/UserData.java
@@ -3,10 +3,10 @@ package com.bruno13palhano;
 public interface UserData<T extends User> {
     void insert(T user);
     void update(T user);
-    void delete(Long userId);
+    void delete(Integer userId);
     T getByUsername(String username);
     Boolean usernameAlreadyExist(String username);
     Boolean emailAlreadyExist(String email);
     T getByEmail(String email);
-    T getById(Long userId);
+    T getById(Integer userId);
 }
